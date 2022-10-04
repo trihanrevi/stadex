c0c6ee36bfd5ba2fb02505e0a8a2c516e8db19b5
trihanrevi <113533158+trihanrevi@users.noreply.github.com>
2022-10-04 22:17:20 +0700

    Update app.r

--- app.r
+++ app.r
@@ -123,7 +123,8 @@ server <- function(input, output, session){
       title = "Statistika Deskriptif",
       digits = 1,
       out = "table1.txt",
-      median = TRUE)
+      median = TRUE,
+      iqr = TRUE)
   )
   output$note <- renderText("STADEX atau Statistical Data Exploration merupakan aplikasi yang berguna untuk melakukan eksplorasi data dan memberikan informasi terkait statistika deskriptif seperti mean, median, max, min, percentil, dan standar deviasi sehingga membantu dalam memahami karakteristik dari data. Statistical Data Exploration juga dapat melakukan uji normalitas untuk melihat kenormalan dari data. Statistical Data Exploration hanya terbatas pada data bertipe numerik.
 ")
